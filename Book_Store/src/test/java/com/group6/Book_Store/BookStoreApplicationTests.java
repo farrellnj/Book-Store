31fad3378d8bd9dcd59697d48ae1d8e17effccdc
c4cnewhaven <naugyanks@gmail.com>
2022-02-06 17:14:51 -0500

    first commit

---
+++ Book_Store/src/test/java/com/group6/Book_Store/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.Book_Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
